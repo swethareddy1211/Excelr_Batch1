ada9117d29ee5aca1217ce185833755215a4075e
swethareddy1211 <gandaswethareddy@gmail.com>
2025-02-07 17:37:38 +0530

    Print array elements

---
+++ Core_Java/Demo065.java
@@ -0,0 +1,10 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
