e7469976598ad93a195c6f92476433b0a7028be5
swethareddy1211 <gandaswethareddy@gmail.com>
2025-02-11 11:22:38 +0530

    Circle Area

--- Core_Java/Demo013.java
+++ Core_Java/Demo013.java
@@ -1,19 +1,16 @@
-public class Demo013{
-    public class CircleArea {
-        public static void main(String[] args) {
-            
-            double radius = 5.0;
-            
-            
-            double area = Math.PI * Math.pow(radius, 2);
-    
-        
-            System.out.println("The area of the circle is: " + area);
-        }
+import java.util.Scanner;
+
+public class Demo013 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle: ");
+        double r = sc.nextDouble();
+
+        double area = (r * r * 3.14);
+
+        System.out.println("Area of Circle: " + area);
+
     }
-    
-    
+
 }
-    
-    
-    
