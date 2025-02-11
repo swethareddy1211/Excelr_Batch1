0b98d15817d6f7e7acf0e9bb4bc1eca1e07fcae1
swethareddy1211 <gandaswethareddy@gmail.com>
2025-02-11 11:30:09 +0530

    Sum of all the elements of an array

--- Core_Java/Demo069.java
+++ Core_Java/Demo069.java
@@ -1,16 +1,17 @@
+class ArraySum{
+    int sum(int[] arr){
+        int[] newArr = arr; int sum = 0;
+        for (int i = 0; i< newArr.length; i++){  
+            sum = sum += newArr[i];
+        } 
+        return sum;
+    }
+}
 public class Demo069 {
     public static void main(String[] args) {
-        // Define and initialize an array
-        int[] array = {1, 2, 3, 4, 5};
-        
-        // Variable to store the sum of elements
-        int sum = 0;
-        
-        // Loop through the array and add each element to the sum
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        // Print the sum of the elements
-        System.out.println("The sum of all elements in the array is: " + sum);
+        ArraySum obj = new ArraySum();
+        int[] arr = {10,50,80,90};
+        int sumOfArray = obj.sum(arr);
+        System.out.println(sumOfArray);
     }
 }
