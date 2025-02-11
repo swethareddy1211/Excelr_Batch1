62cdad7be5b710b7bf7fa221be0a190d51dadcde
swethareddy1211 <gandaswethareddy@gmail.com>
2025-02-11 11:20:43 +0530

    Inheritance with child class and parent class

--- Core_Java/Demo011.java
+++ Core_Java/Demo011.java
@@ -1,24 +1,26 @@
-public class Demo011 {
-    public class SwapNumbers {
-        public static void main(String[] args) {
-            // Declare and initialize two variables
-            int num1 = 10;
-            int num2 = 20;
-    
-            System.out.println("Before Swap:");
-            System.out.println("num1 = " + num1);
-            System.out.println("num2 = " + num2);
-    
-            
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
-    
-            System.out.println("After Swap:");
-            System.out.println("num1 = " + num1);
-            System.out.println("num2 = " + num2);
-        }
+class Animal {
+    String name;
+    public Animal(String name) {
+        this.name = name;
     }
-    
-    
+    public void makeSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says Woof!");
+    }
+}
+public class Demo011 {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal");
+        animal.makeSound();
+        Dog dog = new Dog("Buddy");
+        dog.makeSound();
+    }
 }
